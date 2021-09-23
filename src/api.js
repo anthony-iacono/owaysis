31578015d6a7e3e6f1f87ac413fd5fb9a32df19c
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-09-22 18:34:28 -0700

    Add getSingleCustomer

--- src/api.js
+++ src/api.js
@@ -1,13 +1,30 @@
 const api = {
-  const getAllCustomers = () => {
-    fetch('http://localhost:3001/api/v1/customers') {
+  getAllCustomers() {
+    fetch('http://localhost:3001/api/v1/customers')
       .then(response => {
         if (!response.ok) {
-          throw new Error(`HTTP error! status; ${response.status}`)
+          throw new Error(`HTTP error! status: ${response.status}`);
         }
-        return response;
+
+        return response.json();
       })
-    }
+      .then(allCustomers => {
+        console.log(allCustomers);
+      });
+  },
+
+  getSingleCustomer(id) {
+    fetch(`http://localhost:3001/api/v1/customers/${id}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        return response.json();
+      })
+      .then(singleCustomer => {
+        console.log(singleCustomer);
+      });
   }
 }
 
