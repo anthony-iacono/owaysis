92bb0239ef007805950670ab38944ab62247376a
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-09-24 10:24:07 -0700

    Export dom.js to scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -3,13 +3,19 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+import dom from './dom';
 import api from './api';
 
-api.getAllCustomers();
-api.getCustomer(1);
-api.getAllRooms();
-api.getAllBookings();
-const newBooking = api.addBooking(1, '2021/10/01', 1);
-const deleteBooking = newBooking.then(newBooking => {
-  api.deleteBooking(newBooking.id);
-});
+
+
+
+
+// How to use data returned from a fetch call:
+// window.onload = getCustomer5;
+// function getCustomer5() {
+  //   const data = api.getCustomer(5)
+  //     .then(data => {
+    //       console.log(data);
+    //       customerName.innerText = data.name;
+    //   })
+    // }
