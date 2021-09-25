132688828697b4bafc3f1789dbaf76551445da76
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-09-25 12:31:39 -0700

    Add bookings param

--- src/classes/Hotel.js
+++ src/classes/Hotel.js
@@ -1,5 +1,5 @@
 class Hotel {
-  constructor(customers, rooms) {
+  constructor(customers, rooms, bookings) {
     this.customers = customers;
     this.rooms = rooms;
     this.bookings = bookings;
