03829d7f9bde3ca7c48f558de197f686076fb230
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-09-25 16:09:06 -0700

    Add dom helper functions

--- src/dom.js
+++ src/dom.js
@@ -1,10 +1,23 @@
 const dom = {
-  show(...elements) {
-    elements.forEach(element => element.classList.remove('hidden'));
+  
+  displayCustomerDashboard() {
+    show(select('js-customer-dashboard'));
   },
 
   hide(...elements) {
     elements.forEach(element => element.classList.add('hidden'));
+  },
+
+  select(element) {
+    document.querySelector(element);
+  },
+
+  show(...elements) {
+    elements.forEach(element => element.classList.remove('hidden'));
+  },
+
+  updateHeader(text) {
+    select.header().innerText = `${text}`;
   }
 }
 
