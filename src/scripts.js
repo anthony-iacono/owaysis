cbab9f16e8d8d718a7138967144c9e4799cfe9a0
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-09-25 13:43:40 -0700

    Add data to top.hotel

--- src/scripts.js
+++ src/scripts.js
@@ -12,20 +12,8 @@ import Hotel from './classes/Hotel';
 const top = {};
 
 window.onload = () => {
-  const customers = api.getAllCustomers();
-  const rooms = api.getAllRooms();
-  const bookings = api.getAllBookings();
-  top.hotel = new Hotel(customers, rooms, bookings);
-  console.log(top.hotel);
+  Promise.all([api.getAllCustomers(), api.getAllRooms(), api.getAllBookings()])
+    .then(data => {
+      top.hotel = new Hotel(data[0], data[1], data[2]);
+    })
 }
-
-
-// How to use data returned from a fetch call:
-// window.onload = getCustomer5;
-// function getCustomer5() {
-  //   const data = api.getCustomer(5)
-  //     .then(data => {
-    //       console.log(data);
-    //       customerName.innerText = data.name;
-    //   })
-    // }
