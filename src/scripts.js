cdb5c1523f86e09c904571e010aff20156951137
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-09-25 16:03:12 -0700

    Add organizing comments

--- src/scripts.js
+++ src/scripts.js
@@ -1,35 +1,48 @@
-import './css/base.scss';
+// CSS Imports /////////////////////////////////////////////////////////////////
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
+import './css/base.scss';
 import './images/turing-logo.png'
+
+// Variable Imports ////////////////////////////////////////////////////////////
 
 import select from './select';
 import dom from './dom';
 import api from './api';
 
+// Class Imports ///////////////////////////////////////////////////////////////
+
 import Customer from './classes/Customer';
 import Hotel from './classes/Hotel';
+
+// Selectors ///////////////////////////////////////////////////////////////////
+
+const header = dom.select('.js-header');
+const loginErrorMessage = dom.select('.js-login-error-message');
 
 const top = {};
 
 window.onload = () => {
-  Promise.all([api.getAllCustomers(), api.getAllRooms(), api.getAllBookings()])
+  Promise.all([api.getAllCustomers(), api.getAllRooms(), api.getAllBookings(), api.addBooking(5, 234123, 5)])
     .then(data => {
       top.hotel = new Hotel(data[0], data[1], data[2]);
+      console.log(data[3]);
     })
 }
 
+// Functions ///////////////////////////////////////////////////////////////////
+
 const logIn = () => {
   event.preventDefault();
-  const username = select.usernameField().value;
-  const userID = select.usernameField().value.slice(9);
+  select.loginErrorMessage().innerText = '';
+  const username = dom.select('.js-username-field').value;
+  const userID = parseInt(username.slice(8));
   const customer = top.hotel.customers.find(customer => customer.id === userID)
   if (customer) {
     top.user = new Customer(username, customer.id, customer.name)
   } else if (username === 'manager') {
     top.user = new Manager(username);
   } else {
-    select.loginErrorMessage().innerText = 'Sorry, the username or password you entered is not recognized. Please try again.'
+    loginErrorMessage.innerText = 'Sorry, the username or password you entered is not recognized. Please try again.'
   }
 }
 
