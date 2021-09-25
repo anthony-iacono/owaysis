26432cdb584f1675e656d2462ec1eec3e8d7610a
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-09-25 14:49:45 -0700

    Add show/hide functions

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,11 @@
 const dom = {
+  show(...elements) {
+    elements.forEach(element => element.classList.remove('hidden'));
+  },
 
+  hide(...elements) {
+    elements.forEach(element => element.classList.add('hidden'));
+  }
 }
 
 export default dom;
