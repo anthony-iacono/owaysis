812408699c8d5084a7c0de5bcdd2b5eb96bfa927
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-09-25 17:12:46 -0700

    Remove getCustomerInfo

--- src/classes/Customer.js
+++ src/classes/Customer.js
@@ -6,13 +6,6 @@ class Customer extends User {
     this.id = id;
     this.name = name;
   }
-
-  getCustomerInfo(id, customers) {
-    this.id = parseInt(id);
-    console.log(customers)
-    this.name = customers.find(customer => customer.id === this.id).name;
-    console.log(this.id, this.name);
-  }
 }
 
 export default Customer;
